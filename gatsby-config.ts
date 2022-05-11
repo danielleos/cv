7dc4925965bc4569bbd0d57ec5744cfdb9babfa6
danielleos <danielle.oh.es@gmail.com>
2022-05-11 23:01:54 +0100

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `cv`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-theme-ui"]
+};
+
+export default config;
